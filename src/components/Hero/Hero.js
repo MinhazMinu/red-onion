0a3580f795adfc02f2f4d4c72e8efd334388ff32
minhaz <minhaz.taher@gmail.com>
2020-03-22 14:55:21 +0600

    hero section complite

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="d-flex align-items-center text-center banner hero-section-img">
+      <div className="container">
+        <h1>Best Food Waiting For Your Belly!</h1>
+
+        <div className="search col-md-6 my-5 mx-auto d-flex ">
+          <input
+            type="text"
+            className="form-control mr-4 p-3"
+            placeholder="Search your food!"
+          />
+          <button className="btn btn-round btn-search">Search</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
