dffa40c6d7f77758e73e2a86c90089d71b81106d
minhaz <minhaz.taher@gmail.com>
2020-03-22 13:28:08 +0600

    starting Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
-
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// Adding All COmponents
+
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Foods from "./components/Foods/Foods";
+import AllFeatures from "./components/AllFeatures/AllFeatures";
+import FoodDetails from "./components/FoodDetails/FoodDetails";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div className="globalStyle">
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/login"></Route>
+          <Route exact path="/">
+            <Hero></Hero>
+            <Foods></Foods>
+            <AllFeatures></AllFeatures>
+          </Route>
+          <Route path="/food/:id">
+            <FoodDetails></FoodDetails>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
